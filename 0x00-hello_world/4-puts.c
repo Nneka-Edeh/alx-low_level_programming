140ea1772d739e7283863413569cd658d18bf5ec
Nneka-Edeh <treasuredneky@gmail.com>
2022-12-08 14:07:36 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,22 @@
 #!/bin/bash
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                            
-		        return (0);)
-}
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (success)
+ *
+ *     */
 
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+
+
+}
