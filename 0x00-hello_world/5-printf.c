2a10d311a2777160fb20ae6d5d35582679ccde6a
Nneka-Edeh <treasuredneky@gmail.com>
2022-12-11 13:13:22 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	printf("With proper grammar, but the outcome is a place of art,\n");
+	printf("with proper grammar, but the outcome is a place of art,\n");
 	return (0);
 
 }
